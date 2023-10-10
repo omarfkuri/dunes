29f4158e1ec1bedf1a29cf8e562aeb016d488de3
Omar <omarfkuri@gmail.com>
2023-10-09 19:33:54 -0600

    Export JSXOptions

--- packages/bab/src/types/index.ts
+++ packages/bab/src/types/index.ts
@@ -5,3 +5,11 @@ export { type NodePath, type Scope, type TraverseOptions } from "@babel/traverse
 export { type File, type Node } from "@babel/types";
 
 export { ParseResult, ParserOptions } from "@babel/parser";
+
+export interface JSXOptions {
+  useBuiltIns?: boolean
+  useSpread?: boolean
+  pragma?: string
+  pragmaFrag?: string
+  runtime?: "classic" | (string & ({}))
+}
