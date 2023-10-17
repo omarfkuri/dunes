e114fc128664021c4b435dff6b1907abb3473bf6
Omar <omarfkuri@gmail.com>
2023-10-16 18:06:50 -0600

    Added bundle

--- packages/site/src/bin/builder.ts
+++ packages/site/src/bin/builder.ts
@@ -1,95 +1,117 @@
 #!/usr/bin/env node
 
-import { verify } from "@dunes/verify";
-import { resolve } from "path";
-import type { BuilderConfig } from "src/index.js";
+import { Bundler } from "@dunes/bundle";
 
 const [,,SCRIPT_NAME = "build.js"] = process.argv;
 
-let config;
-const configPath = resolve(SCRIPT_NAME);
-try {
-  ({default: config} = await import(configPath));
-}
-catch(error) {
-  console.log("Could not find", configPath);
-  process.exit(0);
-}
 
-if (!config || typeof config !== "object") {
-  throw "No config";
-}
+const bundler = new Bundler({});
 
-try {
-  verify<BuilderConfig>(config, {
-    options: {
-      prop: {
-        out: ["string", "undefined"],
-        src: ["string", "undefined"],
-        lib: ["string", "undefined"],
-        main: ["string", "undefined"],
-        base: ["string", "undefined"],
 
-        hash: ["string", "undefined", "null"],
-        assets: [{
-          prop: {
-            source: "string",
-            out: ["string", "undefined"],
-          }
-        }, "null"],
-        views: {
-          prop: {
-            folder: "string",
-            only: "object"
-          }
-        },
-        css: [{
-          prop: {
-            file: "string",
-            ext: "string",
-            match: "object",
-            transform: "function",
-          }
-        }, "null"],
-        wrap: {
-          prop: {
+const bundle = await bundler.bundleScript("export const n = 33;");
+const code = await bundle.code();
 
-          }
-        }
-      }
-    },
+console.log({code})
 
-    build: {
-      prop: {
-        clean: ["boolean", "undefined"]
-      }
-    },
 
-    watch: {
-      prop: {
-        onFileBuilding: ["function", "undefined"],
-        onFileBuilt: ["function", "undefined"],
-        onFileFailure: ["function", "undefined"],
+
+
+
+
+
+
+
+
+
+// import { verify } from "@dunes/verify";
+// import { resolve } from "path";
+// import type { BuilderConfig } from "src/index.js";
+
+
+// let config;
+// const configPath = resolve(SCRIPT_NAME);
+// try {
+//   ({default: config} = await import(configPath));
+// }
+// catch(error) {
+//   console.log("Could not find", configPath);
+//   process.exit(0);
+// }
+
+// if (!config || typeof config !== "object") {
+//   throw "No config";
+// }
+
+// try {
+//   verify<BuilderConfig>(config, {
+//     options: {
+//       prop: {
+//         out: ["string", "undefined"],
+//         src: ["string", "undefined"],
+//         lib: ["string", "undefined"],
+//         main: ["string", "undefined"],
+//         base: ["string", "undefined"],
+
+//         hash: ["string", "undefined", "null"],
+//         assets: [{
+//           prop: {
+//             source: "string",
+//             out: ["string", "undefined"],
+//           }
+//         }, "null"],
+//         views: {
+//           prop: {
+//             folder: "string",
+//             only: "object"
+//           }
+//         },
+//         css: [{
+//           prop: {
+//             file: "string",
+//             ext: "string",
+//             match: "object",
+//             transform: "function",
+//           }
+//         }, "null"],
+//         wrap: {
+//           prop: {
+
+//           }
+//         }
+//       }
+//     },
+
+//     build: {
+//       prop: {
+//         clean: ["boolean", "undefined"]
+//       }
+//     },
+
+//     watch: {
+//       prop: {
+//         onFileBuilding: ["function", "undefined"],
+//         onFileBuilt: ["function", "undefined"],
+//         onFileFailure: ["function", "undefined"],
         
-        onDepStart: ["function", "undefined"],
-        onDepBuilding: ["function", "undefined"],
-        onDepBuilt: ["function", "undefined"],
-        onDepFailure: ["function", "undefined"],
-        onDepFinish: ["function", "undefined"],
-      }
-    },
+//         onDepStart: ["function", "undefined"],
+//         onDepBuilding: ["function", "undefined"],
+//         onDepBuilt: ["function", "undefined"],
+//         onDepFailure: ["function", "undefined"],
+//         onDepFinish: ["function", "undefined"],
+//       }
+//     },
 
-    produce: {
-      prop: {
-        origin: "string",
-        do: ["object", "undefined"]
-      }
-    }
-  });
-  console.log("Builder config is ok!");
-}
-catch(error) {
-  console.log("There was an error");
-  console.warn(error);
-  process.exit();
-}
+//     produce: {
+//       prop: {
+//         origin: "string",
+//         do: ["object", "undefined"]
+//       }
+//     }
+//   });
+//   console.log("Builder config is ok!");
+// }
+// catch(error) {
+//   console.log("There was an error");
+//   console.warn(error);
+//   process.exit();
+// }
