ac6e248105c827288ac2e75b5b29509aedb5776c
Omar <omarfkuri@gmail.com>
2023-10-17 02:10:14 -0600

    Fixed test

--- packages/lang/test/index.ts
+++ packages/lang/test/index.ts
@@ -1,5 +1,5 @@
 import { js } from "@dunes/tools";
-import { JSParser } from "../src/langs/js";
+import { JSParser } from "../src/langs/js/index.js";
 import { c } from "@dunes/sys"
 
 
@@ -12,11 +12,11 @@ try {
 
 	 `);
 
-  c.log(result.program)
+  console.log(js(result.program))
 }
 catch(err) {
-	c.red.log("ERROR")
-	c.gray.log(err + " " + js(jsParser.body));
+	console.log("ERROR")
+	console.log(err + " " + js(jsParser.body));
 }
 
 /* Cannot use keyword as member expression */
