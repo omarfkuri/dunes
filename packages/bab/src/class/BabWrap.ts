7362d340cb374311ef0874d3d16f76b91135ae4b
Omar <omarfkuri@gmail.com>
2023-10-16 19:58:21 -0600

    Added default transform options

--- packages/bab/src/class/BabWrap.ts
+++ packages/bab/src/class/BabWrap.ts
@@ -10,28 +10,40 @@ import type {
 
 
 export class BabWrap {
-  constructor(readonly parseOpts: ParserOptions) {}
+  #opts: TransformOptions | undefined
+  constructor(
+    readonly parseOpts: ParserOptions,
+    opts: TransformOptions | undefined
+  ) {
+    this.#opts = opts;
+  }
 
   read(source: string): Bab {
-    return new Bab(parser.parse(source, this.parseOpts));
+    return new Bab(parser.parse(source, this.parseOpts), this.#opts);
   }
 
   from(node: Node): Bab {
     const ast = parser.parse("", this.parseOpts);
     ast.program.body.push(node as any)
-    return new Bab(ast);
+    return new Bab(ast, this.#opts);
   }
 }
 
 class Bab {
-  constructor(readonly result: ParseResult<File>) {}
+  #opts: TransformOptions | undefined
+  constructor(
+    readonly result: ParseResult<File>, 
+    opts: TransformOptions | undefined
+  ) {
+    this.#opts = opts;
+  }
 
   traverse(options: TraverseOptions, scope?: Scope, parent?: NodePath) {
     traverse(this.result, options, scope, parent);
   }
 
-  convert(opts: TransformOptions) {
-    return transformFromAstSync(this.result, undefined, opts);
+  convert(opts?: TransformOptions) {
+    return transformFromAstSync(this.result, undefined, {...this.#opts, ...opts});
   }
 
 }
